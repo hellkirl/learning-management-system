74154268c7f250f46c48d9fead3ca5bba61fe556
hellkirl <=>
2024-05-14 19:58:41 +0300

    replace env vars

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -1,4 +1,5 @@
+import config from '../../config/prod';
+
 export const jwtConstants = {
-  secret:
-    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+  secret: config.jwtSecret,
 };
